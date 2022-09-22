251ec39d83e3f475065ffef7eaea2e29647f8dd6
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-09-22 06:40:15 -0300

    requisitos 2 e 4 concluidos

--- script.js
+++ script.js
@@ -5,9 +5,14 @@
 
 /**
  * Função responsável por criar e retornar o elemento de imagem do produto.
+ * 
  * @param {string} imageSource - URL da imagem.
  * @returns {Element} Elemento de imagem do produto.
  */
+
+// import { fetchProducts } from './helpers/fetchProducts';
+const poeNoCarrinho = document.getElementsByClassName('cart__items')[0]; // tag do tipo <ol>
+
 const createProductImageElement = (imageSource) => {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -49,12 +54,30 @@ const createProductItemElement = ({ id, title, thumbnail }) => {
   return section;
 };
 
+const callCreateProductItemElement = async () => {
+  const object = await fetchProducts('computador');
+  const arrayComputers = object.results;
+  const addItemSection = document.getElementsByClassName('items');
+  // alert(addItemSection);
+  arrayComputers.forEach((computer) => {
+   const { id, title, thumbnail } = computer;
+   const createItem = createProductItemElement({ id, title, thumbnail });
+   addItemSection[0].appendChild(createItem);// https://stackoverflow.com/questions/31861190/getelementsbyclass-and-appendchild
+  }); 
+
+//   console.log(object);
+//   return object;
+};
+// const actual = productItemElement();
+// actual.then((data) => {
+// console.log(data);
+// });
 /**
  * Função que recupera o ID do produto passado como parâmetro.
  * @param {Element} product - Elemento do produto.
  * @returns {string} ID do produto.
  */
-const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
+// const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
 
 /**
  * Função responsável por criar e retornar um item do carrinho.
@@ -64,12 +87,35 @@ const getIdFromProductItem = (product) => product.querySelector('span.id').inner
  * @param {string} product.price - Preço do produto.
  * @returns {Element} Elemento de um item do carrinho.
  */
+function cartItemClickListener(li) {
+//   const liDoProduto = li.parentElement.firstChild.innerText;
+//   alert(liDoProduto);
+alert(li);
+}
+
 const createCartItemElement = ({ id, title, price }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `ID: ${id} | TITLE: ${title} | PRICE: $${price}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener(li));
   return li;
 };
 
-window.onload = () => { };
+function callCreateCartItemElement() {
+ const botoesAddCart = document.querySelectorAll('.item__add');
+// vou pegar todos os botoes add ao carrinho e mapea-los com forEach
+  botoesAddCart.forEach((botao) => {
+   botao.addEventListener('click', async () => {
+    const idDoProduto = botao.parentElement.firstChild.innerText;
+    // alert(id);
+    const dadosDoProduto = await fetchItem(idDoProduto);
+    // alert(fetchItemProduto);
+    poeNoCarrinho.appendChild(createCartItemElement(dadosDoProduto));
+   });
+  });
+}
+
+window.onload = async () => { 
+  await callCreateProductItemElement();
+  callCreateCartItemElement();
+};
